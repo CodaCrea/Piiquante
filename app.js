5db06cac15b5ae76ddc72e2d129ff05d722873da
devveric <asusgtx1050@outlook.fr>
2023-01-06 16:44:09 +0100

    06012023

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+app.use((req, res, next) => {
+  res.status(201);
+  next();
+});
+
+app.use((req, res) => {
+  res.json({ message: 'Connexion établie' });
+});
+
+module.exports = app;
