25132abcc8b4016db28fe53e04a0070850516fc9
l Asus l <eric.bt@outlook.fr>
2023-01-12 08:01:51 +0100

    12012023

--- app.js
+++ app.js
@@ -1,13 +1,31 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const userRoute = require('./routes/user');
+const sauceRoute = require('./routes/sauce');
+const path = require('path');
 const app = express();
+require('dotenv').config();
 
-app.use((req, res, next) => {
-  res.status(201);
+mongoose.set('strictQuery', true),
+  mongoose.connect(process.env.mongoDB,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    })
+    .then(() => console.log('Connexion établie à MongoDB'))
+    .catch(() => console.log('Erreur de connexion à MongoDB'));
+
+app.use(express.json());
+
+app.use('/api/auth', (req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
   next();
 });
 
-app.use((req, res) => {
-  res.json({ message: 'Connexion établie' });
-});
+app.use('/api/auth', userRoute);
+app.use('/api/sauces', sauceRoute);
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
 module.exports = app;
---
+++ controllers/user.js
@@ -0,0 +1,58 @@
+const bcrypt = require('bcrypt');
+const jsonWebToken = require('jsonwebtoken');
+const User = require('../models/User');
+
+// exports.signup = async (req, res) => {
+//   try {
+//     const hash = bcrypt.hash(req.body.password, 10);
+//     const user = await new User({
+//       email: req.body.email,
+//       password: hash
+//     });
+//     if (!user) {
+//       throw new Error(
+//         `Veuillez remplir les champs email et mot de passe ${res.status(400)}`
+//       );
+//     } else {
+//       user.save();
+//       res.status(201).json({ message: 'Utilisateur créé !' });
+//     }
+//   } catch (error) {
+//     res.status(500).json({ error });
+//   }
+// };
+
+exports.signup = (req, res, next) => {
+  bcrypt.hash(req.body.password, 10)
+    .then(hash => {
+      const user = new User({
+        email: req.body.email,
+        password: hash
+      });
+      user.save()
+        .then(() => res.status(201).json({ message: 'Utilisateur créé !' }))
+        .catch(error => res.status(400).json({ error }));
+    })
+    .catch(error => res.status(500).json({ error }));
+};
+
+exports.login = async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    const valid = await bcrypt.compare(res.body.password, user.password);
+    if (!user || !valid) {
+      throw new Error(`email ou mot de passe incorrect ${res.status(401)}`);
+    } else {
+      res.status(200).json({
+        userId: user._id,
+        token: jsonWebToken.sign(
+          { userId: user._id },
+          process.env.TOKEN_KEY,
+          { expiresIn: '24h' }
+        )
+      });
+    }
+  } catch (error) {
+    res.status(500).json({ error });
+  }
+};
