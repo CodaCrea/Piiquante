7fd9b73869c2796496e1dc8beda8e2d3719cb20c
l Asus l <eric.bt@outlook.fr>
2023-01-23 09:30:32 +0100

    23012023

---
+++ models/Password.js
@@ -0,0 +1,21 @@
+const passwordValidator = require('password-validator');
+
+const passwordSchema = new passwordValidator();
+
+passwordSchema
+// Longueur minimum de 8 caractères
+.is().min(8)
+// Longueur maximum de 100 caractères
+.is().max(20)
+// Doit contenir au moins une lettre en majuscule
+.has().uppercase(1)
+// Doit contenir des lettres en minuscule
+.has().lowercase()
+// Doit contenir au moins un chiffre
+.has().digits(1)
+// Ne doit pas contenir d'espace
+.has().not().spaces()
+// Les valeurs étants sur liste noire
+.is().not().oneOf(['Passw0rd', 'Password123', 'Motdepasse0']);
+
+module.exports = passwordSchema;
--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,17 @@
 const express = require('express');
+const rateLimit = require('express-rate-limit');
 const userController = require('../controllers/user');
+const Password = require('../models/Password');
 const router = express.Router();
 
+const  limiter  =  rateLimit ({
+	windowMs : 15  *  60  *  1000,  // 15 minutes
+	max : 100,  // Limite chaque IP à 100 requêtes par `window` (ici, par 15 minutes)
+	standardHeaders : true,  // Return rate limit info dans les en-têtes `RateLimit-*`
+	legacyHeaders : true,  // Active les en-têtes `X-RateLimit-*`
+});
 
 router.post('/signup', userController.signup);
-router.post('/login', userController.login);
+router.post('/login', limiter, userController.login);
 
 module.exports = router;
